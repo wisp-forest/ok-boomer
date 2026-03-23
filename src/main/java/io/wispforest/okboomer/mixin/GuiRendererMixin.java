1f83f5cd9fc36d154427741d080f0b74f012a49d
glisco <glisco03@web.de>
2026-03-23 20:51:40 +0100

    1.21.11

--- src/main/java/io/wispforest/okboomer/mixin/GuiRendererMixin.java
+++ src/main/java/io/wispforest/okboomer/mixin/GuiRendererMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(GuiRenderer.class)
 public class GuiRendererMixin {
 
-    @ModifyArg(method = "renderPreparedDraws", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/DynamicUniforms;write(Lorg/joml/Matrix4fc;Lorg/joml/Vector4fc;Lorg/joml/Vector3fc;Lorg/joml/Matrix4fc;F)Lcom/mojang/blaze3d/buffers/GpuBufferSlice;"), index = 0)
+    @ModifyArg(method = "renderPreparedDraws", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/DynamicUniforms;write(Lorg/joml/Matrix4fc;Lorg/joml/Vector4fc;Lorg/joml/Vector3fc;Lorg/joml/Matrix4fc;)Lcom/mojang/blaze3d/buffers/GpuBufferSlice;"), index = 0)
     public Matrix4fc injectRenderTransform(Matrix4fc modelView) {
         var transform = new Matrix4f();
         transform.mul(modelView);
